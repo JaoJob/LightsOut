f52ff6f343586163559b4eec3e83ec9785fd0182
Личак Михаил <mihail.lichak@gmail.com>
2018-11-22 12:59:11 +0300

    Initial commit

---
+++ LightsOut/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label25 = new System.Windows.Forms.Label();
+            this.Label24 = new System.Windows.Forms.Label();
+            this.Label21 = new System.Windows.Forms.Label();
+            this.Label23 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label22 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label20 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label19 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label18 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label17 = new System.Windows.Forms.Label();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.Label16 = new System.Windows.Forms.Label();
+            this.Label8 = new System.Windows.Forms.Label();
+            this.Label15 = new System.Windows.Forms.Label();
+            this.Label9 = new System.Windows.Forms.Label();
+            this.Label14 = new System.Windows.Forms.Label();
+            this.Label10 = new System.Windows.Forms.Label();
+            this.Label13 = new System.Windows.Forms.Label();
+            this.Label11 = new System.Windows.Forms.Label();
+            this.Label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label25
+            // 
+            this.Label25.BackColor = System.Drawing.Color.Yellow;
+            this.Label25.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label25.Location = new System.Drawing.Point(324, 285);
+            this.Label25.Name = "Label25";
+            this.Label25.Size = new System.Drawing.Size(60, 60);
+            this.Label25.TabIndex = 25;
+            this.Label25.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label24
+            // 
+            this.Label24.BackColor = System.Drawing.Color.Yellow;
+            this.Label24.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label24.Location = new System.Drawing.Point(264, 285);
+            this.Label24.Name = "Label24";
+            this.Label24.Size = new System.Drawing.Size(60, 60);
+            this.Label24.TabIndex = 24;
+            this.Label24.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label21
+            // 
+            this.Label21.BackColor = System.Drawing.Color.Yellow;
+            this.Label21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label21.Location = new System.Drawing.Point(84, 285);
+            this.Label21.Name = "Label21";
+            this.Label21.Size = new System.Drawing.Size(60, 60);
+            this.Label21.TabIndex = 21;
+            this.Label21.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label23
+            // 
+            this.Label23.BackColor = System.Drawing.Color.Yellow;
+            this.Label23.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label23.Location = new System.Drawing.Point(204, 285);
+            this.Label23.Name = "Label23";
+            this.Label23.Size = new System.Drawing.Size(60, 60);
+            this.Label23.TabIndex = 23;
+            this.Label23.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label1
+            // 
+            this.Label1.BackColor = System.Drawing.Color.Yellow;
+            this.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label1.Location = new System.Drawing.Point(84, 45);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(60, 60);
+            this.Label1.TabIndex = 1;
+            this.Label1.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label22
+            // 
+            this.Label22.BackColor = System.Drawing.Color.Yellow;
+            this.Label22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label22.Location = new System.Drawing.Point(144, 285);
+            this.Label22.Name = "Label22";
+            this.Label22.Size = new System.Drawing.Size(60, 60);
+            this.Label22.TabIndex = 22;
+            this.Label22.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label2
+            // 
+            this.Label2.BackColor = System.Drawing.Color.Yellow;
+            this.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label2.Location = new System.Drawing.Point(144, 45);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(60, 60);
+            this.Label2.TabIndex = 2;
+            this.Label2.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label3
+            // 
+            this.Label3.BackColor = System.Drawing.Color.Yellow;
+            this.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label3.Location = new System.Drawing.Point(204, 45);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(60, 60);
+            this.Label3.TabIndex = 3;
+            this.Label3.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label20
+            // 
+            this.Label20.BackColor = System.Drawing.Color.Yellow;
+            this.Label20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label20.Location = new System.Drawing.Point(324, 225);
+            this.Label20.Name = "Label20";
+            this.Label20.Size = new System.Drawing.Size(60, 60);
+            this.Label20.TabIndex = 20;
+            this.Label20.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label4
+            // 
+            this.Label4.BackColor = System.Drawing.Color.Yellow;
+            this.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label4.Location = new System.Drawing.Point(264, 45);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(60, 60);
+            this.Label4.TabIndex = 4;
+            this.Label4.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label19
+            // 
+            this.Label19.BackColor = System.Drawing.Color.Yellow;
+            this.Label19.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label19.Location = new System.Drawing.Point(264, 225);
+            this.Label19.Name = "Label19";
+            this.Label19.Size = new System.Drawing.Size(60, 60);
+            this.Label19.TabIndex = 19;
+            this.Label19.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label5
+            // 
+            this.Label5.BackColor = System.Drawing.Color.Yellow;
+            this.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label5.Location = new System.Drawing.Point(324, 45);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(60, 60);
+            this.Label5.TabIndex = 5;
+            this.Label5.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label18
+            // 
+            this.Label18.BackColor = System.Drawing.Color.Yellow;
+            this.Label18.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label18.Location = new System.Drawing.Point(204, 225);
+            this.Label18.Name = "Label18";
+            this.Label18.Size = new System.Drawing.Size(60, 60);
+            this.Label18.TabIndex = 18;
+            this.Label18.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label6
+            // 
+            this.Label6.BackColor = System.Drawing.Color.Yellow;
+            this.Label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label6.Location = new System.Drawing.Point(84, 105);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(60, 60);
+            this.Label6.TabIndex = 6;
+            this.Label6.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label17
+            // 
+            this.Label17.BackColor = System.Drawing.Color.Yellow;
+            this.Label17.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label17.Location = new System.Drawing.Point(144, 225);
+            this.Label17.Name = "Label17";
+            this.Label17.Size = new System.Drawing.Size(60, 60);
+            this.Label17.TabIndex = 17;
+            this.Label17.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label7
+            // 
+            this.Label7.BackColor = System.Drawing.Color.Yellow;
+            this.Label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label7.Location = new System.Drawing.Point(144, 105);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(60, 60);
+            this.Label7.TabIndex = 7;
+            this.Label7.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label16
+            // 
+            this.Label16.BackColor = System.Drawing.Color.Yellow;
+            this.Label16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label16.Location = new System.Drawing.Point(84, 225);
+            this.Label16.Name = "Label16";
+            this.Label16.Size = new System.Drawing.Size(60, 60);
+            this.Label16.TabIndex = 16;
+            this.Label16.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label8
+            // 
+            this.Label8.BackColor = System.Drawing.Color.Yellow;
+            this.Label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label8.Location = new System.Drawing.Point(204, 105);
+            this.Label8.Name = "Label8";
+            this.Label8.Size = new System.Drawing.Size(60, 60);
+            this.Label8.TabIndex = 8;
+            this.Label8.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label15
+            // 
+            this.Label15.BackColor = System.Drawing.Color.Yellow;
+            this.Label15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label15.Location = new System.Drawing.Point(324, 165);
+            this.Label15.Name = "Label15";
+            this.Label15.Size = new System.Drawing.Size(60, 60);
+            this.Label15.TabIndex = 15;
+            this.Label15.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label9
+            // 
+            this.Label9.BackColor = System.Drawing.Color.Yellow;
+            this.Label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label9.Location = new System.Drawing.Point(264, 105);
+            this.Label9.Name = "Label9";
+            this.Label9.Size = new System.Drawing.Size(60, 60);
+            this.Label9.TabIndex = 9;
+            this.Label9.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label14
+            // 
+            this.Label14.BackColor = System.Drawing.Color.Yellow;
+            this.Label14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label14.Location = new System.Drawing.Point(264, 165);
+            this.Label14.Name = "Label14";
+            this.Label14.Size = new System.Drawing.Size(60, 60);
+            this.Label14.TabIndex = 14;
+            this.Label14.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label10
+            // 
+            this.Label10.BackColor = System.Drawing.Color.Yellow;
+            this.Label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label10.Location = new System.Drawing.Point(324, 105);
+            this.Label10.Name = "Label10";
+            this.Label10.Size = new System.Drawing.Size(60, 60);
+            this.Label10.TabIndex = 10;
+            this.Label10.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label13
+            // 
+            this.Label13.BackColor = System.Drawing.Color.Yellow;
+            this.Label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label13.Location = new System.Drawing.Point(204, 165);
+            this.Label13.Name = "Label13";
+            this.Label13.Size = new System.Drawing.Size(60, 60);
+            this.Label13.TabIndex = 13;
+            this.Label13.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label11
+            // 
+            this.Label11.BackColor = System.Drawing.Color.Yellow;
+            this.Label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label11.Location = new System.Drawing.Point(84, 165);
+            this.Label11.Name = "Label11";
+            this.Label11.Size = new System.Drawing.Size(60, 60);
+            this.Label11.TabIndex = 11;
+            this.Label11.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // Label12
+            // 
+            this.Label12.BackColor = System.Drawing.Color.Yellow;
+            this.Label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label12.Location = new System.Drawing.Point(144, 165);
+            this.Label12.Name = "Label12";
+            this.Label12.Size = new System.Drawing.Size(60, 60);
+            this.Label12.TabIndex = 12;
+            this.Label12.Click += new System.EventHandler(this.ClickLabel);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 50;
+            this.button1.Text = "Еще раз";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 432);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Label25);
+            this.Controls.Add(this.Label24);
+            this.Controls.Add(this.Label21);
+            this.Controls.Add(this.Label23);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Label22);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label20);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label19);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label18);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label17);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.Label16);
+            this.Controls.Add(this.Label8);
+            this.Controls.Add(this.Label15);
+            this.Controls.Add(this.Label9);
+            this.Controls.Add(this.Label14);
+            this.Controls.Add(this.Label10);
+            this.Controls.Add(this.Label13);
+            this.Controls.Add(this.Label11);
+            this.Controls.Add(this.Label12);
+            this.Name = "Form1";
+            this.Text = "LightsOut";
+            this.Load += new System.EventHandler(this.button1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label25;
+        internal System.Windows.Forms.Label Label24;
+        internal System.Windows.Forms.Label Label21;
+        internal System.Windows.Forms.Label Label23;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Label Label22;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label20;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label19;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label18;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label17;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Label Label16;
+        internal System.Windows.Forms.Label Label8;
+        internal System.Windows.Forms.Label Label15;
+        internal System.Windows.Forms.Label Label9;
+        internal System.Windows.Forms.Label Label14;
+        internal System.Windows.Forms.Label Label10;
+        internal System.Windows.Forms.Label Label13;
+        internal System.Windows.Forms.Label Label11;
+        internal System.Windows.Forms.Label Label12;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LightsOut/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int[] arrayOnOff = new int[25] ;
+        public int score = 0;
+
+       
+        private void ClickLabel(object sender, EventArgs e)
+        {                                                                                //обработка нажатия на любое поле
+            Label tempLabel = (Label)sender;
+            int a = tempLabel.TabIndex;
+            score += 1;
+            arrayOnOff[a - 1] *= -1;
+            if (a > 5) arrayOnOff[a - 6] *= -1;                                             //меняем состояние соседних полей, если нужно
+            if (a <= 20) arrayOnOff[a + 4] *= -1;
+            if (a != 5 && a != 10 && a != 15 && a != 20 && a != 25) arrayOnOff[a] *= -1;
+            if (a != 1 && a != 6 && a != 11 && a != 16 && a != 21) arrayOnOff[a - 2] *= -1;
+            RefreshLabels();
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();                                                      //устанавливаем все поля в случайные состояния
+            for (int i = 0; i <= 24; i++)
+            {   
+                arrayOnOff[i] = rnd.Next(2) == 0 ? -1 : 1; 
+            }
+            RefreshLabels();
+            score = 0;
+
+        }
+        public void RefreshLabels()
+        {
+            int res = 0;
+            for (int i = 1; i <= 25; i++)                                                   //обновляем состояние всех полей
+            {
+                string vr = "label" + i.ToString();
+
+                if (arrayOnOff[i - 1] == -1) this.Controls[vr].BackColor = Color.Black;
+                if (arrayOnOff[i - 1] == 1) this.Controls[vr].BackColor = Color.Yellow;
+                res += arrayOnOff[i - 1];
+                if (res == 25) { MessageBox.Show(score + " ходов"); break; }
+            }
+        }
+
+    }
+}
